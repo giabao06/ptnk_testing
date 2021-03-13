901951766594030f88991fa4218cc4649d5e5626
giabao06 <giabaodoremon@gmail.com>
2021-03-13 22:33:56 +0700

    thêm bt1 của đề 2019-2020

---
+++ 19-20/symprime.pas
@@ -0,0 +1,57 @@
+program symprime;
+const filein='symprime.inp'; fileout='symprime.out';
+var i:INTEGER; n:int64; t:ARRAY[1..100] OF int64; f:text;
+
+function snt(x:int64):boolean;
+var i:INTEGER;
+begin
+   if x<2 then exit(false);
+   for i:=2 to trunc(sqrt(x)) do
+   if x mod i=0 then exit(false);
+   exit(true);
+end;
+
+procedure fread;
+begin
+  assign(f,filein);
+  reset(f);
+  readln(f,n);
+  if n>100000 then
+  begin
+    close(f);
+    assign(f,fileout);
+    rewrite(f);
+    writeln(f,'n>10^5');
+    close(f);
+    exit;
+  end;
+  for i:=1 to n do readln(f,t[i]);
+  close(f);
+  //for i:=1 to n do write(t[i],#32); //debug only
+end;
+
+procedure proc;
+var fr,bk,tmp:int64;
+begin
+  fr:=0; bk:=0; tmp:=0;
+  assign(f,fileout);
+  rewrite(f);
+  for i:=1 to n do
+  if (snt(t[i])) and (t[i]<20000000) then
+  begin
+    fr:=t[i]+1;
+    bk:=t[i]-1;
+    repeat fr:=fr+1 until snt(fr);
+    repeat bk:=bk-1 until snt(bk);
+    tmp:=(fr+bk) div 2;
+    //writeln(tmp);
+    if tmp=t[i] then writeln(f,'YES') else writeln(f,'NO')
+  end
+  else writeln(f,'Ko phai snt hoac lon hon 2*10^7');
+  close(f);
+end;
+
+begin
+  fread;
+  proc;
+end.
